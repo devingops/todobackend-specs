9524bd1268e1fe85026e276b5095d2784543471b
devingops <scottmccracken1995@gmail.com>
2017-12-27 16:09:30 +0000

    removed test

--- test/tests.js
+++ test/tests.js
@@ -8,28 +8,6 @@ chai.use(chaiAsPromised);
 var url = process.env.URL || 'http://localhost:8000/todos';
 
 
-describe('Cross Origin Requests', function() {
-  var result;
-
-  before(function() {
-      result = request('OPTIONS', url)
-        .set('Origin', 'http://someplace.com')
-        .end();
-  });
-
-  it('should return the correct CORS headers', function() {
-    return assert(result, "header").to.contain.all.keys([
-      'access-control-allow-origin',
-      'access-control-allow-methods',
-      'access-control-allow-headers'
-    ]);
-  });
-
-  it('should allow all origins', function() {
-    return assert(result, "header.access-control-allow-origin").to.equal('*');
-  });
-});
-
 describe('Create Todo Item', function() {
   var result;
 
